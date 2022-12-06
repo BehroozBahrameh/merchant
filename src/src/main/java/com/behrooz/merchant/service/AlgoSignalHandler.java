6511191c9f3c4e688fdd1f49b17cfa1a9fda3384
Behrooz Bahrameh <behrooz.bahrameh@gmail.com>
2022-12-06 21:15:00 +0100

    introduce AlgoSignalHandler

---
+++ src/src/main/java/com/behrooz/merchant/service/AlgoSignalHandler.java
@@ -0,0 +1,39 @@
+package com.behrooz.merchant.service;
+
+import com.behrooz.merchant.tradingalgo.Algo;
+import com.behrooz.merchant.tradingalgo.SignalHandler;
+
+public class AlgoSignalHandler implements SignalHandler {
+    public void handleSignal(int signal) {
+        Algo algo = new Algo();
+
+        switch (signal) {
+            case 1:
+                algo.setUp();
+                algo.setAlgoParam(1, 60);
+                algo.performCalc();
+                algo.submitToMarket();
+                break;
+
+            case 2:
+                algo.reverse();
+                algo.setAlgoParam(1, 80);
+                algo.submitToMarket();
+                break;
+
+            case 3:
+                algo.setAlgoParam(1, 90);
+                algo.setAlgoParam(2, 15);
+                algo.performCalc();
+                algo.submitToMarket();
+                break;
+
+            default:
+                algo.cancelTrades();
+                break;
+        }
+
+        algo.doAlgo();
+    }
+
+}
