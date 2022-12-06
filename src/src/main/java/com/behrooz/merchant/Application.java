10359866a2e944cc0393d8485c407fe86cf492a4
Behrooz Bahrameh <behrooz.bahrameh@gmail.com>
2022-12-06 21:02:01 +0100

    rename MerchantApplication to Application

--- src/src/main/java/com/behrooz/merchant/Application.java
+++ src/src/main/java/com/behrooz/merchant/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MerchantApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MerchantApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
