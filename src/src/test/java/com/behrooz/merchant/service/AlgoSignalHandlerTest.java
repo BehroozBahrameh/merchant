6ae103f79811a4849c3a3d1c7a66442c6c82722a
Behrooz Bahrameh <behrooz.bahrameh@gmail.com>
2022-12-07 21:22:44 +0100

    [feat] check the exact value for signales

--- src/src/test/java/com/behrooz/merchant/service/AlgoSignalHandlerTest.java
+++ src/src/test/java/com/behrooz/merchant/service/AlgoSignalHandlerTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.util.Pair;
 
-import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.*;
 
 @DisplayName("AlgoSignalHandler Test")
@@ -25,7 +25,6 @@ public class AlgoSignalHandlerTest {
         MockitoAnnotations.initMocks(this);
 
         doNothing().when(algo).setUp();
-        doNothing().when(algo).setAlgoParam(isA(Integer.class), isA(Integer.class));
         doNothing().when(algo).performCalc();
         doNothing().when(algo).submitToMarket();
         doNothing().when(algo).doAlgo();
@@ -35,13 +34,15 @@ public class AlgoSignalHandlerTest {
     public void handleSignal_SignalCode1_CalledAlgoAppropriately() {
         //Arrange
         int signal = 1;
+        var param = Pair.of(1, 60);
+        doNothing().when(algo).setAlgoParam(param.getFirst(), param.getSecond());
 
         //Act
         handler.handleSignal(signal);
 
         //Assert
+        verify(algo, times(1)).setAlgoParam(param.getFirst(), param.getSecond());
         verify(algo, times(1)).setUp();
-        verify(algo, times(1)).setAlgoParam(isA(Integer.class), isA(Integer.class));
         verify(algo, times(1)).performCalc();
         verify(algo, times(1)).submitToMarket();
         verify(algo, times(1)).doAlgo();
@@ -54,13 +55,15 @@ public class AlgoSignalHandlerTest {
     public void handleSignal_SignalCode2_CalledAlgoAppropriately() {
         //Arrange
         int signal = 2;
+        var param = Pair.of(1, 80);
+        doNothing().when(algo).setAlgoParam(param.getFirst(), param.getSecond());
 
         //Act
         handler.handleSignal(signal);
 
         //Assert
+        verify(algo, times(1)).setAlgoParam(param.getFirst(), param.getSecond());
         verify(algo, times(1)).reverse();
-        verify(algo, times(1)).setAlgoParam(isA(Integer.class), isA(Integer.class));
         verify(algo, times(1)).submitToMarket();
         verify(algo, times(1)).doAlgo();
 
@@ -73,12 +76,18 @@ public class AlgoSignalHandlerTest {
     public void handleSignal_SignalCode3_CalledAlgoAppropriately() {
         //Arrange
         int signal = 3;
+        var firstParam = Pair.of(1, 90);
+        var secondParam = Pair.of(2, 15);
+
+        doNothing().when(algo).setAlgoParam(firstParam.getFirst(), firstParam.getSecond());
+        doNothing().when(algo).setAlgoParam(secondParam.getFirst(), secondParam.getSecond());
 
         //Act
         handler.handleSignal(signal);
 
         //Assert
-        verify(algo, times(2)).setAlgoParam(isA(Integer.class), isA(Integer.class));
+        verify(algo, times(1)).setAlgoParam(firstParam.getFirst(), firstParam.getSecond());
+        verify(algo, times(1)).setAlgoParam(secondParam.getFirst(), secondParam.getSecond());
         verify(algo, times(1)).performCalc();
         verify(algo, times(1)).submitToMarket();
         verify(algo, times(1)).doAlgo();
