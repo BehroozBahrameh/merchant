9b532cab49240cc2d69bf7e97da8eef849617d03
Behrooz Bahrameh <behrooz.bahrameh@gmail.com>
2022-12-07 21:01:11 +0100

    [feat] cover signal 3

--- src/src/test/java/com/behrooz/merchant/service/AlgoSignalHandlerTest.java
+++ src/src/test/java/com/behrooz/merchant/service/AlgoSignalHandlerTest.java
@@ -68,4 +68,22 @@ public class AlgoSignalHandlerTest {
         verify(algo, times(0)).performCalc();
         verify(algo, times(0)).cancelTrades();
     }
+
+    @Test
+    public void handleSignal_SignalCode3_CalledAlgoAppropriately() {
+        //Arrange
+        int signal = 3;
+
+        //Act
+        handler.handleSignal(signal);
+
+        //Assert
+        verify(algo, times(2)).setAlgoParam(isA(Integer.class), isA(Integer.class));
+        verify(algo, times(1)).performCalc();
+        verify(algo, times(1)).submitToMarket();
+        verify(algo, times(1)).doAlgo();
+
+        verify(algo, times(0)).setUp();
+        verify(algo, times(0)).cancelTrades();
+    }
 }
