b8b4ca348f3cbc28ee13baa99edda400ca3c7e93
Behrooz Bahrameh <behrooz.bahrameh@gmail.com>
2022-12-08 06:32:27 +0100

    [refactor][factory-pattern] second signal handler
    responsible for sognal=2

---
+++ src/src/main/java/com/behrooz/merchant/service/signalhandler/SecondSignalHandler.java
@@ -0,0 +1,15 @@
+package com.behrooz.merchant.service.signalhandler;
+
+import org.springframework.data.util.Pair;
+
+public class SecondSignalHandler extends BaseSignalHandler {
+
+    private static final Pair<Integer, Integer> SECOND_SIGNAL_PARAM = Pair.of(1, 80);
+
+    @Override
+    void process() {
+        algo.reverse();
+        algo.setAlgoParam(SECOND_SIGNAL_PARAM.getFirst(), SECOND_SIGNAL_PARAM.getSecond());
+        algo.submitToMarket();
+    }
+}
---
+++ src/src/test/java/com/behrooz/merchant/service/SecondSignalHandlerTest.java
@@ -0,0 +1,47 @@
+package com.behrooz.merchant.service;
+
+import com.behrooz.merchant.service.signalhandler.SecondSignalHandler;
+import com.behrooz.merchant.tradingalgo.Algo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.util.Pair;
+
+import static org.mockito.Mockito.*;
+
+public class SecondSignalHandlerTest {
+    @Mock
+    private Algo algo;
+
+    @InjectMocks
+    private SecondSignalHandler signalHandler;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+
+        doNothing().when(algo).doAlgo();
+    }
+
+    @Test
+    public void handleSignal_WhenCall_CalledAlgoAppropriately() {
+        //Arrange
+        var param = Pair.of(1, 80);
+
+        //Act
+        signalHandler.handleSignal();
+
+        //Assert
+        verify(algo, times(1)).setAlgoParam(isA(Integer.class), isA(Integer.class));
+        verify(algo, times(1)).setAlgoParam(param.getFirst(), param.getSecond());
+        verify(algo, times(1)).reverse();
+        verify(algo, times(1)).submitToMarket();
+        verify(algo, times(1)).doAlgo();
+
+        verify(algo, times(0)).setUp();
+        verify(algo, times(0)).performCalc();
+        verify(algo, times(0)).cancelTrades();
+    }
+}
