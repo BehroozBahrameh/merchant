1ea8bc981ed72ab71298e48f0df1146435973db6
Behrooz Bahrameh <behrooz.bahrameh@gmail.com>
2022-12-08 06:39:57 +0100

    [refactor][factory-pattern] third signal handler
    responsible for sognal=3

---
+++ src/src/main/java/com/behrooz/merchant/service/signalhandler/ThirdSignalHandler.java
@@ -0,0 +1,17 @@
+package com.behrooz.merchant.service.signalhandler;
+
+import org.springframework.data.util.Pair;
+
+public class ThirdSignalHandler extends BaseSignalHandler {
+
+    private static final Pair<Integer, Integer> THIRD_SIGNAL_PARAM_1 = Pair.of(1, 90);
+    private static final Pair<Integer, Integer> THIRD_SIGNAL_PARAM_2 = Pair.of(2, 15);
+
+    @Override
+    void process() {
+        algo.setAlgoParam(THIRD_SIGNAL_PARAM_1.getFirst(), THIRD_SIGNAL_PARAM_1.getSecond());
+        algo.setAlgoParam(THIRD_SIGNAL_PARAM_2.getFirst(), THIRD_SIGNAL_PARAM_2.getSecond());
+        algo.performCalc();
+        algo.submitToMarket();
+    }
+}
---
+++ src/src/test/java/com/behrooz/merchant/service/ThirdSignalHandlerTest.java
@@ -0,0 +1,49 @@
+package com.behrooz.merchant.service;
+
+import com.behrooz.merchant.service.signalhandler.ThirdSignalHandler;
+import com.behrooz.merchant.tradingalgo.Algo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.util.Pair;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.*;
+
+public class ThirdSignalHandlerTest {
+    @Mock
+    private Algo algo;
+
+    @InjectMocks
+    private ThirdSignalHandler signalHandler;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+
+        doNothing().when(algo).doAlgo();
+    }
+
+    @Test
+    public void handleSignal_WhenCall_CalledAlgoAppropriately() {
+        //Arrange
+        var firstParam = Pair.of(1, 90);
+        var secondParam = Pair.of(2, 15);
+
+        //Act
+        signalHandler.handleSignal();
+
+        //Assert
+        verify(algo, times(2)).setAlgoParam(isA(Integer.class), isA(Integer.class));
+        verify(algo, times(1)).setAlgoParam(firstParam.getFirst(), firstParam.getSecond());
+        verify(algo, times(1)).setAlgoParam(secondParam.getFirst(), secondParam.getSecond());
+        verify(algo, times(1)).performCalc();
+        verify(algo, times(1)).submitToMarket();
+        verify(algo, times(1)).doAlgo();
+
+        verify(algo, times(0)).setUp();
+        verify(algo, times(0)).cancelTrades();
+    }
+}
