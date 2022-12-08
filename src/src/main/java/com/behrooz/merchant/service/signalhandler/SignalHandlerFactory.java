cefc01da744efb6d561f9ac2c7eb66a7d30b8d6e
Behrooz Bahrameh <behrooz.bahrameh@gmail.com>
2022-12-08 17:52:02 +0100

    [feat] switch-case less factory!

--- src/src/main/java/com/behrooz/merchant/service/signalhandler/SignalHandlerFactory.java
+++ src/src/main/java/com/behrooz/merchant/service/signalhandler/SignalHandlerFactory.java
@@ -1,20 +1,28 @@
 package com.behrooz.merchant.service.signalhandler;
 
-import com.behrooz.merchant.service.signalhandler.components.*;
+import com.behrooz.merchant.service.signalhandler.components.BaseSignalHandler;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignalHandlerFactory {
+    private static final String DefaultSignalHandler = "signal-processor-default";
+    private static final String SignalHandlerPattern = "signal-processor-%s";
+    private @Autowired AutowireCapableBeanFactory beanFactory;
+
     public BaseSignalHandler GetHandler(int signal) {
-        switch (signal) {
-            case 1:
-                return new FirstSignalHandler();
-            case 2:
-                return new SecondSignalHandler();
-            case 3:
-                return new ThirdSignalHandler();
-            default:
-                return new DefaultSignalHandler();
+        BaseSignalHandler instance;
+        try {
+            instance = (BaseSignalHandler) beanFactory.getBean(getName(signal));
+        } catch (NoSuchBeanDefinitionException exception) {
+            instance = (BaseSignalHandler) beanFactory.getBean(DefaultSignalHandler);
         }
+        return instance;
+    }
+
+    public static String getName(int signal) {
+        return SignalHandlerPattern.formatted(signal);
     }
 }
